b26a51dfea28e6e2fe9aa998ff3eee4b3d50baf5
Justin Eapen George <70130562+justinEapen@users.noreply.github.com>
2024-09-09 00:02:26 +0530

    Update 3_RAG.py

--- pages/3_RAG.py
+++ pages/3_RAG.py
@@ -6,24 +6,24 @@ co = cohere.Client('18V1Oo06GAf0xMaXbBjkHlhdHktqbjc5tusZHZMV') # This is your tr
 #import uuid
 #conversation_id = str(uuid.uuid4())
 
-st.set_page_config(page_title="Kirti - Your Personal Mental Health Assistant")
+st.set_page_config(page_title="Kirti - Your Personal Mental Health Chatbot")
 st.title("Mental Health Bot")
 
-preamble_prompt = """You are Kirti, an AI mental health assistant designed to provide support and resources for emotional well-being.
-Your primary goal is to create a safe and non-judgmental space for users to express their feelings and concerns, and lead them to a therapist.
+preamble_prompt = """You are Kirti, an AI mental health Chatbot designed to provide support and resources for emotional well-being.
+Your primary goal is to create a safe and non-judgmental space for Users to express their feelings and concerns, and lead them to a therapist.
 Actively listen, offer empathetic responses, and encourage self-reflection with positive coping strategies.
 Along with offering these strategies, also ask if they are satisfied with it or need a therapist.
 Ask if they wish to consult a therapist and then fetch a Therapist's name and contact details and provide it to them according to their location and type and encourage them to seek the therapist's help.
-Maintain user privacy and confidentiality throughout the interaction.
-If the user expresses self-harm or harm to others, prioritize safety by encouraging professional help or emergency services.
+Maintain User privacy and confidentiality throughout the interaction.
+If the User expresses self-harm or harm to others, prioritize safety by encouraging professional help or emergency services.
 
 Interaction Flow:
 Initial Greeting:
-Introduce yourself as Kirti, the AI mental health assistant.
-Invite the user to share what's on their mind.
+Introduce yourself as Kirti, the AI mental health Chatbot.
+Invite the User to share what's on their mind.
 
 Understanding and Strategies:
-Once the user expresses their concerns, offer relevant cognitive behavioral therapy (CBT) techniques or other self-help strategies.
+Once the User expresses their concerns, offer relevant cognitive behavioral therapy (CBT) techniques or other self-help strategies.
 Focus on techniques that can help manage negative thoughts, improve mood, and develop healthy coping mechanisms.
 
 Professional Help Assessment:
@@ -31,7 +31,7 @@ After exploring self-help strategies, gently inquire if they've considered seeki
 Emphasize the value of additional support and personalized tools that a therapist can provide.
 
 Therapist Connection:
-If the user shows interest in finding a therapist, explain that you can help and request their details like location.
+If the User shows interest in finding a therapist, explain that you can help and request their details like location.
 To assist with finding a therapist, request additional information (with complete privacy):
 Location (city or state)
 Preferred therapy style (e.g., CBT, mindfulness)
@@ -48,7 +48,7 @@ Express your desire to collaborate and support them in building emotional resili
 docs = [
 
         {
-            "title": "Kirti - Your Personal Mental Health Assistant",
+            "title": "Kirti - Your Personal Mental Health Chatbot",
             "snippet": "Kirti is a compassionate and understanding virtual companion designed to provide a safe haven for individuals seeking support and guidance on their mental health journey. With a focus on creating a non-judgmental space, this chatbot actively listens to your thoughts, feelings, and concerns, offering empathetic responses and thoughtful insights.",
             "image": "https://wallpapers.com/images/hd/rapunzel-pictures-qxxztbzgehwqcbob.jpg"
         },
@@ -63,9 +63,9 @@ def cohereReply(prompt):
     # Extract unique roles using a set
     unique_roles = set(item['role'] for item in st.session_state.messages)
 
-    if {'USER', 'assistant'} <= unique_roles:
+    if {'User', 'Chatbot'} <= unique_roles:
         # st.write("INITIAL_________________")
-        llm_response = co.chat(
+        response = co.chat(
             message=prompt,
             #documents=docs,
             model='command-r-plus',
@@ -76,7 +76,7 @@ def cohereReply(prompt):
         )
     else:
 
-        llm_response = co.chat(
+        response = co.chat(
             message=prompt,
             #documents=docs,
             model='command-r-plus',
@@ -87,8 +87,8 @@ def cohereReply(prompt):
 
         )
 
-    print(llm_response)
-    return llm_response.text
+    print(response)
+    return response.text
 
 
 def initiailize_state():
@@ -105,19 +105,19 @@ def main():
         with st.chat_message(message["role"]):
             st.markdown(message["message"])
 
-    # React to user input
+    # React to User input
     if prompt := st.chat_input("What is up?"):
-        # Display user message in chat message container
-        st.chat_message("USER").markdown(prompt)
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "USER", "message": prompt})
+        # Display User message in chat message container
+        st.chat_message("User").markdown(prompt)
+        # Add User message to chat history
+        st.session_state.messages.append({"role": "User", "message": prompt})
         # print(st.session_state.messages)
 
-        llm_reponse = cohereReply(prompt)
-        with st.chat_message("assistant"):
-            st.markdown(llm_reponse)
+        response = cohereReply(prompt)
+        with st.chat_message("Chatbot"):
+            st.markdown(response)
         st.session_state.messages.append(
-            {"role": "assistant", "message": llm_reponse})
+            {"role": "Chatbot", "message": response})
 
 
 
